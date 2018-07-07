303466fd2912af1719f89d2c9d97df7cbb7aace6
remi <root@localhost.localdomain>
2018-07-07 17:29:01 +0200

    Add bot exemple

---
+++ bot_exemple/bot.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'programr'
+
+brains = Dir.glob("sara_spanish_aiml/bt.aiml")
+
+robot = ProgramR::Facade.new
+robot.learn(brains)
+
+begin
+  print '>> '
+  s = STDIN.gets.chomp
+  
+  if s != 'quit'
+    reaction = robot.get_reaction(s)
+    STDOUT.puts "<< #{reaction}"
+  end
+end while s != 'quit'
